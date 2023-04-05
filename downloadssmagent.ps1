e72f51d35d8f19bf14337d1284e91cd16fdccd0b
Oscar-21 <austin.conder@outlook.com>
2023-04-05 20:32:19 +0000

    updated template

--- downloadssmagent.ps1
+++ downloadssmagent.ps1
@@ -1,16 +1,11 @@
 $progressPreference = 'silentlyContinue'
 $desktop = (Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders').Desktop
-$zip_path = Join-Path -Path $desktop -ChildPath 'aws-schema-conversion-tool-1.0.latest.zip'
-$Url = 'https://s3.amazonaws.com/publicsctdownload/Windows/aws-schema-conversion-tool-1.0.latest.zip'
-$DownloadZipFile = $desktop + $(Split-Path -Path $Url -Leaf)
-Invoke-WebRequest -Uri $Url -OutFile $DownloadZipFile
+$url = 'https://s3.amazonaws.com/publicsctdownload/Windows/aws-schema-conversion-tool-1.0.latest.zip'
+$zip_path = $desktop + $(Split-Path -Path $url -Leaf)
+Invoke-WebRequest -Uri $url -OutFile $zip_path
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-  param([string]$zipfile, [string]$outpath)
-  [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
+function Unzip { param([string]$zipfile, [string]$outpath) [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath) }
 $sct_unzip_path = Join-Path -Path $desktop -ChildPath 'aws-schema-conversion-tool-1.0.latest'
 Unzip $zip_path $sct_unzip_path
 $sct_msi_path =  Join-Path -Path $desktop -ChildPath 'aws-schema-conversion-tool-1.0.latest/AWS Schema Conversion Tool-1.0.671.msi'
-msiexec /i "C:\Users\austi\OneDrive\Desktop\aws-schema-conversion-tool-1.0.latest\AWS Schema Conversion Tool-1.0.671.msi" /q /passive  /l*v "install.log"
+msiexec /i $sct_msi_path /q /passive  /l*v "install.log"
